1adb2f0ac8a7fac909c945ec6d6fe637240e887a
Marcos8060 <marcosgav80@gmail.com>
2022-06-13 12:27:45 +0300

    created a cartcontaier and corresponding cartItem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
-
 
 function App() {
   return <main>
---
+++ src/components/CartContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CartItem from './CartItem'
+
+const CartContainer = () => {
+  return (
+    <div>CartContainer</div>
+  )
+}
+
+export default CartContainer
